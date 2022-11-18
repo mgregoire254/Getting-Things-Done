efb9f9d92eaaf5d84cba3690375041027b3f80e2
mgregoire254 <mgregoire1@me.com>
2022-11-18 13:54:01 -0600

    Added comments and method to add new things

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+#database models
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
@@ -16,7 +17,21 @@ def __repr__(self):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    #Adds new thing to list and attempts to append to database.
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your thing'
+#Displays list of things in database by date created.
+    else:
+        tasks = Todo.query.ordery_by(Todo.date_created).all()
+        return render_template('index.html')
     
 if __name__ == "__main__":
     app.run(debug=True)    
